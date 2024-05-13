35b043e2f9cd415e54340958e5460d3268b95c2a
Davi Soares <davisfs2110@gmail.com>
2024-05-13 20:11:42 -0300

    remove: Apaga o .gitignore

---
+++ Python - Primeira entrega/dist/Interseccao/Interseccao.py
@@ -0,0 +1,37 @@
+# Após receber os conjuntos, o programa insere elementos em comum entre eles no conjunto A^B.
+
+import os
+
+os.system('cls')
+
+conjA = []
+conjB = []
+
+def ins_elementos(conj,nome):
+    while True:
+        elemento = input(f'Insira elementos no conjunto {nome}{conj}(Digite "/" para parar): ')
+        if elemento == '/':
+            os.system('cls')
+            break
+        conj.append(elemento)
+        
+ins_elementos(conjA,"A")
+ins_elementos(conjB,"B")
+
+conjA = set(conjA)
+conjB = set(conjB)
+
+intersAB = sorted(list(conjA & conjB))
+
+print("Conjunto A: ",conjA)
+print("Conjunto B: ",conjB)
+
+print("\n")
+print("Conjunto A ^ B: ",intersAB)
+
+print()
+print("|-----------------------------------------------|")
+print("|             Feito por Davi Soares             |")
+print("|-----------------------------------------------|")
+print()
+input("Pressione ENTER para finalizar a execução")
