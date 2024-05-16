b22cdc6417d6850344ff0b6306cac197e6b793cb
Davi Soares <davisfs2110@gmail.com>
2024-05-15 21:28:33 -0300

    feat: Adiciona o programa que fornece a matriz de igualdade

---
+++ TS - Segunda entrega/matriz.ts
@@ -0,0 +1,55 @@
+function produtoCartesiano(conjA: string[], conjB: string[]): string {
+    var produto: string = '__|_' + conjB[0];
+    for (var j = 1; j < conjB.length; j++) {
+        produto += '_|_' + conjB[j];
+    }
+    
+    produto += "_|";
+
+    for (var i = 0; i < conjA.length; i++) {
+        produto += "\n" + conjA[i] + " | ";
+        for (var j = 0; j < conjB.length; j++) {
+            if (conjA[i] == conjB[j]){
+                produto += "1 | ";
+            } else {
+                produto += "0 | ";
+            }
+        }
+    }
+    return produto;
+}
+
+var input = require("prompt-sync")();
+
+var vetor1: string[] = [];
+var vetor2: string[] = [];
+var valor: string = '';
+
+console.clear();
+
+console.log("Digite os valores do conjunto A(digite '/' para parar): ");
+while (true) {
+    valor = '';
+    valor = input('> ');
+    if (valor === '/') {
+        break;
+    }
+    vetor1.push(valor);
+}
+
+console.clear();
+
+console.log("Digite os valores do conjunto B(digite '/' para parar): ");
+while (true) {
+    valor = '';
+    valor = input('> ');
+    if (valor === '/') {
+        break;
+    }
+    vetor2.push(valor);
+}
+
+console.clear();
+
+console.log("A matriz de AxB é:")
+console.log(produtoCartesiano(vetor1.filter((este, i) => vetor1.indexOf(este) === i), vetor2.filter((este, i) => vetor2.indexOf(este) === i)));
--- TS - Segunda entrega/prodCartesiano.ts
+++ TS - Segunda entrega/prodCartesiano.ts
@@ -1,25 +1,25 @@
-function produtoCartesiano(arr1: string[], arr2: string[]): string {
-    let produto: string = '__|___' + arr2[0];
-    for (let j = 1; j < arr2.length; j++) {
+function matriz(arr1: string[], arr2: string[]): string {
+    var produto: string = '__|___' + arr2[0];
+    for (var j = 1; j < arr2.length; j++) {
         produto += '___|___' + arr2[j];
     }
     
     produto += "___|";
 
-    for (let i = 0; i < arr1.length; i++) {
+    for (var i = 0; i < arr1.length; i++) {
         produto += "\n" + arr1[i] + " | ";
-        for (let j = 0; j < arr2.length; j++) {
+        for (var j = 0; j < arr2.length; j++) {
             produto += "<" + arr1[i] + ";" + arr2[j] + "> | ";
         }
     }
     return produto;
 }
 
-let input = require("prompt-sync")();
+var input = require("prompt-sync")();
 
-let vetor1: string[] = [];
-let vetor2: string[] = [];
-let valor: string = '';
+var vetor1: string[] = [];
+var vetor2: string[] = [];
+var valor: string = '';
 
 console.clear();
 
@@ -48,4 +48,4 @@ while (true) {
 console.clear();
 
 console.log("O produto cartesiano de A com B é: ")
-console.log(produtoCartesiano(vetor1.filter((este, i) => vetor1.indexOf(este) === i), vetor2.filter((este, i) => vetor2.indexOf(este) === i)));
+console.log(matriz(vetor1.filter((este, i) => vetor1.indexOf(este) === i), vetor2.filter((este, i) => vetor2.indexOf(este) === i)));
